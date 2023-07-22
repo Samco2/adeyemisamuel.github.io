78c1298b590e27a8ddd315763e94ae66cd917b35
Samuel Ayobami ADEYEMI <46057925+Samco2@users.noreply.github.com>
2023-07-22 16:19:55 +0100

    add secret setting

--- samco/settings.py
+++ samco/settings.py
@@ -10,11 +10,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-$a17nq1f*_e51l$&(xv#%(prx2=d2751z&ch_gi%(m=)*5c2es')
+SECRET_KEY = os.environ.get('PORT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
